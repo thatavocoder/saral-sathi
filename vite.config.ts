462c3cde3342c70accefc5972d84de884d21de5d
Aashka Doshi <aashkadoshi7@gmail.com>
2025-10-20 12:57:05 +0530

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
